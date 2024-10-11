a2d91c34ed8e5fd08e2cc7d4e9ef510d7869dd26
JLosada-Dev <jd.losada.dev@gmail.com>
2024-10-10 20:58:55 -0500

    fix: comillas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className='text-6xl'>VacaWise Fup 2.0</h1>
-      <p className="bg-slate-500">
+      <p className='bg-slate-500'>
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam molestias
         neque ipsam minima voluptatum perferendis nostrum voluptas. Libero totam
         nemo officiis saepe id, cum ullam ut odit perferendis numquam commodi!
