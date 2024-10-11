c6c60ee6d0f26070e7df131a25ef882596f70978
GitDevCamilo <camilomenesebermeo@gmail.com>
2024-10-10 20:34:36 -0500

    update: Actualizacion rapida para probar el funcionamiento

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
   return (
     <>
-      <h1 className='text-6xl'>VacaWise Fup 2.0</h1>
+      <h1 className='text-6xl'>VacaWise Fup 3.0</h1>
+      <p className="pt-5">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+        voluptate esse id reprehenderit voluptatem quam animi, quo nam cumque,
+        corporis deserunt. Vel quo obcaecati ducimus blanditiis iusto deserunt
+        perspiciatis eum!
+      </p>
     </>
   );
 }
