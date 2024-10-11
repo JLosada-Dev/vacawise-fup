5fbcc09b4802ab52ba50eff0f285f3340496e14a
JLosada-Dev <jd.losada.dev@gmail.com>
2024-10-10 20:43:48 -0500

    test ramas

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,11 @@ function App() {
   return (
     <>
       <h1 className='text-6xl'>VacaWise Fup 2.0</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam molestias
+        neque ipsam minima voluptatum perferendis nostrum voluptas. Libero totam
+        nemo officiis saepe id, cum ullam ut odit perferendis numquam commodi!
+      </p>
     </>
   );
 }
