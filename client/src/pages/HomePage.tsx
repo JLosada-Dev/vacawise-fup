f983191899a8b4941f89155a9222985be5bd995b
GitDevCamilo <camilomenesebermeo@gmail.com>
2024-10-25 15:55:34 -0500

    chore: commit de prueba para verificar conflictos

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -162,13 +162,14 @@ function HomePage() {
             <div className='mx-auto max-w-6xl px-6 lg:px-8'>
               <div className='relative flex flex-col items-center md:grid grid-cols-2 gap-40'>
                 <img
-                  src='/arrow.png'
+                  src='#'
                   className='absolute top-[25rem] md:top-1/2 -translate-y-1/2 z-10 left-1/2 -translate-x-1/2 rotate-90 md:rotate-0'
                 />
 
                 <div className='relative h-80 md:h-full w-full md:justify-self-end max-w-sm rounded-xl bg-gray-900/5 ring-inset ring-gray-900/10 lg:rounded-2xl'>
                   <img
-                    src='/farmer.png'
+                    src='#'
+                    alt='tabla de datos'
                     className='rounded-sm object-cover shadow-md ring-1 ring-gray-900/10 h-full w-full'
                   />
                 </div>
